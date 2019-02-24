ae38b82452d2f7d27e3698d55afe0cc7032e952d
Laura <Lhuiefiction@gmail.com>
2019-02-24 17:41:38 -0500

    Added Karrar's user data to project

---
+++ MovieDbAPI/Models/UserID.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieDbAPI.Models
+{
+    public class UserID
+    {
+        [Required]
+        [RegularExpression(@"^[A-Z{1}]+[a-zA-z{1,30}]+$", ErrorMessage = "Please enter a valid name.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Incorrect E-mail Format!")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public UserID(string userName, string email, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
+
+
